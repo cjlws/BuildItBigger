91637da3aaebdae6ef2ba00d8e5c1c7cb97c9622
johnrobinson <john@cjlws.co.uk>
2016-11-11 10:10:23 +0000

    Test Push of Updates from AS

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
+    // GitHub Test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
